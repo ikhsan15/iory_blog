8f8b077b2d85b488e5d0340bdb3c5ec87b721b22
ikhsan15 <ikhsan15nur@gmail.com>
2021-11-10 22:35:33 +0700

    update link

--- views/posts/post_portal.php
+++ views/posts/post_portal.php
@@ -5,79 +5,77 @@
   <title>CodePen - Article Deck</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel="stylesheet" href="./style.css">
-
 </head>
 <body>
-<!-- partial:index.partial.html -->
-<div class="container">
-  <div class="card" style="--cards:5">
-  <a href="index.php">
-    <div class="child">
-      <h2>Why do I need a will</h2>
-      <p>5 articles</p>
+  <!-- partial:index.partial.html -->
+  <div class="container">
+    <div class="card" style="--cards:5">
+    <a href="../../index.php">
+      <div class="child">
+        <h2>Back to Home</h2>
+        <p>home :)</p>
+      </div>
+      </a>
     </div>
-    </a>
-  </div>
-  <div class="card" style="--cards:3">
-    <div class="child">
-      <h2>How to choose who inherits your estate</h2>
-      <p>3 articles</p>
+    <div class="card" style="--cards:3">
+      <div class="child">
+        <h2>How to choose who inherits your estate</h2>
+        <p>3 articles</p>
+      </div>
+      <div class="child"></div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-    <div class="child"></div>
-  </div>
-  <div class="card" style="--cards:2">
-    <div class="child">
-      <h2>What happens when you die</h2>
-      <p>2 articles</p>
+    <div class="card" style="--cards:2">
+      <div class="child">
+        <h2>What happens when you die</h2>
+        <p>2 articles</p>
+      </div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-  </div>
-  <div class="card" style="--cards:4">
-    <div class="child">
-      <h2>How to choose guardians</h2>
-      <p>4 articles</p>
+    <div class="card" style="--cards:4">
+      <div class="child">
+        <h2>How to choose guardians</h2>
+        <p>4 articles</p>
+      </div>
+      <div class="child"></div>
+      <div class="child"></div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-    <div class="child"></div>
-    <div class="child"></div>
-  </div>
-  <div class="card" style="--cards:4">
-    <div class="child">
-      <h2>Why you've been disinherited</h2>
-      <p>4 articles</p>
+    <div class="card" style="--cards:4">
+      <div class="child">
+        <h2>Why you've been disinherited</h2>
+        <p>4 articles</p>
+      </div>
+      <div class="child"></div>
+      <div class="child"></div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-    <div class="child"></div>
-    <div class="child"></div>
-  </div>
-  <div class="card" style="--cards:3">
-    <div class="child">
-      <h2>How to commit fraud, legally</h2>
-      <p>3 articles</p>
+    <div class="card" style="--cards:3">
+      <div class="child">
+        <h2>How to commit fraud, legally</h2>
+        <p>3 articles</p>
+      </div>
+      <div class="child"></div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-    <div class="child"></div>
-  </div>
-  <div class="card" style="--cards:5">
-    <div class="child">
-      <h2>Stay together for the kids</h2>
-      <p>5 articles</p>
+    <div class="card" style="--cards:5">
+      <div class="child">
+        <h2>Stay together for the kids</h2>
+        <p>5 articles</p>
+      </div>
+      <div class="child"></div>
+      <div class="child"></div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-    <div class="child"></div>
-    <div class="child"></div>
-  </div>
-  <div class="card" style="--cards:3">
-    <div class="child">
-      <h2>Picking music for your funeral</h2>
-      <p>3 articles</p>
+    <div class="card" style="--cards:3">
+      <div class="child">
+        <h2>Picking music for your funeral</h2>
+        <p>3 articles</p>
+      </div>
+      <div class="child"></div>
+      <div class="child"></div>
     </div>
-    <div class="child"></div>
-    <div class="child"></div>
   </div>
-</div>
-<!-- partial -->
-  
-</body>
+  <!-- partial -->
+  </body>
 </html>
