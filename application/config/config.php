0171911963e47ffc83309bbab7f3c71afd1ed54e
ikhsan15 <ikhsan15nur@gmail.com>
2021-12-28 11:13:31 +0700

    testing ssl problem

--- application/config/config.php
+++ application/config/config.php
@@ -30,8 +30,8 @@ if( isset($_SERVER['HTTPS'] ) ) {
 	$config['base_url']  =  "//".$_SERVER['HTTP_HOST'];
 }
 else{
-	// $config['base_url']  =  "//".$_SERVER['HTTP_HOST'];
-	$config['base_url']  =  "http://".$_SERVER['HTTP_HOST'];
+	// $config['base_url']  =  "http://".$_SERVER['HTTP_HOST'];
+	$config['base_url']  =  "//".$_SERVER['HTTP_HOST'];
 }
 $config['base_url'] .= preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])).'/';
 
