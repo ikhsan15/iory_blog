659e457ceac642d7ce2d1dd34d7f1c9a335d7d3a
ikhsan15 <ikhsan15nur@gmail.com>
2022-01-18 17:18:34 +0700

    update home content

--- application/views/index.php
+++ application/views/index.php
@@ -26,32 +26,16 @@
 			<div class="post-preview">
 				<a href="<?php echo base_url(); ?>Main/kata_1">
 					<h2 class="post-title">
-						Man must explore, and this is exploration at its greatest
+						Suaraku
 					</h2>
 					<h3 class="post-subtitle">
-						Problems look mighty small from 150 miles up
+						Terlalu banyak kata yang berlarian dalam pikiranku
 					</h3>
 				</a>
 				<p class="post-meta">
 					Posted by
 					<a href="#!">IORY</a>
-					on September 24, 2021
-				</p>
-			</div>
-			<!-- Divider-->
-			<hr class="my-4" />
-			<!-- Post preview-->
-			<div class="post-preview">
-				<a href="post.php"
-					><h2 class="post-title">
-						I believe every human has a finite number of heartbeats. I don't
-						intend to waste any of mine.
-					</h2></a
-				>
-				<p class="post-meta">
-					Posted by
-					<a href="#!">IORY</a>
-					on September 18, 2021
+					on October 31, 2021
 				</p>
 			</div>
 			<!-- Divider-->
@@ -59,16 +43,17 @@
 			<!-- Post preview-->
 			<div class="post-preview">
 				<a href="post.php">
-					<h2 class="post-title">Science has not yet mastered prophecy</h2>
+					<h2 class="post-title">
+						Bayang
+					</h2>
 					<h3 class="post-subtitle">
-						We predict too much for the next year and yet far too little for the
-						next ten.
+						Terlalu banyak yang hilang
 					</h3>
-				</a>
+					</a>
 				<p class="post-meta">
 					Posted by
 					<a href="#!">IORY</a>
-					on August 24, 2021
+					on October 18, 2021
 				</p>
 			</div>
 			<!-- Divider-->
@@ -76,16 +61,31 @@
 			<!-- Post preview-->
 			<div class="post-preview">
 				<a href="post.php">
-					<h2 class="post-title">Failure is not an option</h2>
+					<h2 class="post-title">Belajar Menghargai</h2>
 					<h3 class="post-subtitle">
-						Many say exploration is part of our destiny, but it’s actually our
-						duty to future generations.
+						Hargai setiap hal kecil yang bisa membuatmu tertawa
 					</h3>
 				</a>
 				<p class="post-meta">
 					Posted by
 					<a href="#!">IORY</a>
-					on July 8, 2021
+					on October 17, 2021
+				</p>
+			</div>
+			<!-- Divider-->
+			<hr class="my-4" />
+			<!-- Post preview-->
+			<div class="post-preview">
+				<a href="post.php">
+					<h2 class="post-title">Bukan Hari Ini</h2>
+					<h3 class="post-subtitle">
+						Pengen a dapatnya b
+					</h3>
+				</a>
+				<p class="post-meta">
+					Posted by
+					<a href="#!">IORY</a>
+					on September 08, 2021
 				</p>
 			</div>
 			<!-- Divider-->
