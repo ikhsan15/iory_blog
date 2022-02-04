3774a52ab4d9377e477d60cf9bb19aa90c7304d4
ikhsan15 <ikhsan15nur@gmail.com>
2022-02-04 15:29:36 +0700

    update content

--- application/views/index.php
+++ application/views/index.php
@@ -26,16 +26,16 @@
 			<div class="post-preview">
 				<a href="<?php echo base_url(); ?>PostLinks/post_2">
 					<h2 class="post-title">
-						Seminggu
+						Takut
 					</h2>
 					<h3 class="post-subtitle">
-						Seminggu tlah berlalu
+						Jangan pergi
 					</h3>
 				</a>
 				<p class="post-meta">
 					Posted by
 					<a href="#!">IORY</a>
-					on January 20, 2022
+					on February 04, 2022
 				</p>
 			</div>
 			<!-- Divider-->
